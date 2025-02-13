e337dfc31f46c3bf84ee28868fb43e51dd6aab56
RngAd33 <yxbsyx@outlook.com>
2025-02-13 23:04:08 +0800

    代码生成器平台项目过程代码maker部分

--- src/main/java/com/yupi/meta/Meta.java
+++ src/main/java/com/yupi/meta/Meta.java
@@ -1,5 +1,53 @@
 package com.yupi.meta;
 
+import java.util.List;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Data
 public class Meta {
 
+    private String name;
+    private String description;
+    private String basePackage;
+    private String version;
+    private String author;
+    private String createTime;
+    private FileConfig fileConfig;
+    private ModelConfig modelConfig;
+
+    @NoArgsConstructor
+    @Data
+    public static class FileConfig {
+        private String inputRootPath;
+        private String outputRootPath;
+        private String type;
+        private List<Files> files;
+
+        @NoArgsConstructor
+        @Data
+        public static class Files {
+            private String inputPath;
+            private String outputPath;
+            private String type;
+            private String generateType;
+        }
+    }
+
+    @NoArgsConstructor
+    @Data
+    public static class ModelConfig {
+        private List<Models> models;
+
+        @NoArgsConstructor
+        @Data
+        public static class Models {
+            private String fieldName;
+            private String type;
+            private String description;
+            private Boolean defaultValue;
+            private String abbr;
+        }
+    }
 }
