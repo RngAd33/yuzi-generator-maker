19220bbbf1b798292e55dc1c27e4a5cb0348fc8c
RngAd33 <yxbsyx@outlook.com>
2025-02-15 22:25:24 +0800

    代码生成器平台项目过程代码maker部分

--- src/main/java/com/yupi/Main.java
+++ src/main/java/com/yupi/Main.java
@@ -1,16 +1,15 @@
 package com.yupi;
 
-import com.yupi.cli.CommandExecutor;
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+// import com.yupi.cli.CommandExecutor;
 
 /**
  * 全局调用入口
  */
 public class Main {
     public static void main(String[] args) {
-        args = new String[]{"generate", "-l", "-a", "-o"};  // 测试语句
-        CommandExecutor commandExecutor = new CommandExecutor();
-        commandExecutor.doExecute(args);
+        // args = new String[]{"generate", "-l", "-a", "-o"};  // 测试语句
+        // CommandExecutor commandExecutor = new CommandExecutor();
+        // commandExecutor.doExecute(args);
+
     }
 }
