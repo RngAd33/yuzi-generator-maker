92af39868bdc08f240cfa03a135cb506a9ce2dcb
RngAd33 <yxbsyx@outlook.com>
2025-02-23 22:54:56 +0800

    Merge remote-tracking branch 'origin/master'

---
+++ src/main/java/com/yupi/meta/enums/FileTypeEnum.java
@@ -0,0 +1,26 @@
+package com.yupi.meta.enums;
+
+public class FileTypeEnum {
+    DIR("目录", "dir"),
+        File("文件", "file");
+
+    private final String text;
+
+    private final String value;
+
+    FileTypeEnum(String text, String value)
+    {
+        this.text = text;
+        this.value = value;
+    }
+
+    private String text()
+    {
+        return text;
+    }
+
+    public String value()
+    {
+        return value;
+    }
+}
