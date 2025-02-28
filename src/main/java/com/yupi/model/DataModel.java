b38c4b6a0c90a30b81c3a09d95eb36df84e5b1fe
RngAd33 <yxbsyx@outlook.com>
2025-02-28 16:39:25 +0800

    Merge remote-tracking branch 'origin/master'

--- src/main/java/com/yupi/model/DataModel.java
+++ src/main/java/com/yupi/model/DataModel.java
@@ -7,20 +7,11 @@ import lombok.Data;
  */
 @Data
 public class DataModel {
+
     /**
      * 是否生成循环（开关）
      */
     public boolean loop;
-
-    /**
-     * 作者注释
-     */
-    public String author = "RngAd33";
-
-    /**
-     * 输出信息
-     */
-    public String outputText = "输出结果";
 
     /**
      * 是否生成 .gitignore 文件
