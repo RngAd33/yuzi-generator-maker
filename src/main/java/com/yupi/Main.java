8c41540e18eac107aa3b73bf4aa458ab49bf5952
RngAd33 <yxbsyx@outlook.com>
2025-03-03 19:06:06 +0800

    Merge remote-tracking branch 'origin/master'

--- src/main/java/com/yupi/Main.java
+++ src/main/java/com/yupi/Main.java
@@ -8,7 +8,6 @@ import java.io.IOException;
  * 全局调用入口
  */
 public class Main {
-
     public static void main(String[] args) throws TemplateException, IOException, InterruptedException {
         MainGenerator mainGenerator = new MainGenerator();
         mainGenerator.doGenerate();
