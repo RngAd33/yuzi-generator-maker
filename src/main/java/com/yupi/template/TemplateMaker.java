417243e873a3cc55de5216f1be418d017edb1a86
RngAd33 <yxbsyx@outlook.com>
2025-03-05 18:18:58 +0800

    Merge remote-tracking branch 'origin/master'

--- src/main/java/com/yupi/template/TemplateMaker.java
+++ src/main/java/com/yupi/template/TemplateMaker.java
@@ -11,7 +11,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * 模板制作工具
+ * 元信息模板制作工具
  */
 public class TemplateMaker {
 
@@ -27,6 +27,7 @@ public class TemplateMaker {
         String sourceRootPath = new File(projectPath).getParent() + File.separator + "yuzi-generator-demo-projects/acm-template-pro";
         String fileInputPath = "src/com/yupi/acm/MainTemplate.java";
         String fileOutputPath = fileInputPath + ".ftl";
+
 
         // 3. 输入模型参数
         Meta.ModelConfig.ModelInfo modelInfo = new Meta.ModelConfig.ModelInfo();
@@ -51,20 +52,20 @@ public class TemplateMaker {
         Meta meta = new Meta();
         meta.setName(name);
         meta.setDescription(description);
-        // fileConfig
+        // (1) fileConfig
         Meta.FileConfig fileConfig = new Meta.FileConfig();
         meta.setFileConfig(fileConfig);
         fileConfig.setSourceRootPath(sourceRootPath);
         List<Meta.FileConfig.FileInfo> fileInfoList = new ArrayList<>();
         fileConfig.setFiles(fileInfoList);
-        // fileInfo
+        // (2) fileInfo
         Meta.FileConfig.FileInfo fileInfo = new Meta.FileConfig.FileInfo();
         fileInfo.setInputPath(fileInputPath);
         fileInfo.setOutputPath(fileOutputPath);
         fileInfo.setType(FileTypeEnum.FILE.getValue());
         fileInfo.setGenerateType(FileGenerateTypeEnum.DYNAMIC.getValue());
         fileInfoList.add(fileInfo);
-        // modelConfig
+        // (3) modelConfig
         Meta.ModelConfig modelConfig = new Meta.ModelConfig();
         meta.setModelConfig(modelConfig);
         List<Meta.ModelConfig.ModelInfo> modelInfoList = new ArrayList<>();
