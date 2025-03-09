99ebe48da40b5154380e59c12c45c69289c53073
RngAd33 <yxbsyx@outlook.com>
2025-03-09 15:33:26 +0800

    Merge remote-tracking branch 'origin/master'

---
+++ src/main/java/com/yupi/template/FileFilter.java
@@ -0,0 +1,74 @@
+package com.yupi.template;
+
+import cn.hutool.core.collection.CollUtil;
+import cn.hutool.core.io.FileUtil;
+import com.yupi.template.enums.FileFilterRangeEnum;
+import com.yupi.template.enums.FileFilterRuleEnum;
+import com.yupi.template.model.FileFilterConfig;
+import java.io.File;
+import java.util.List;
+
+/**
+ * 文件过滤器
+ */
+public class FileFilter {
+
+    /**
+     * 单个文件过滤
+     *
+     * @param fileFilterConfigList 过滤规则
+     * @param file 单个文件
+     * @return 判断是否保留
+     */
+    public static boolean doSingleFileFilter(List<FileFilterConfig> fileFilterConfigList, File file) {
+        String fileName = file.getName();
+        String fileContent = FileUtil.readUtf8String(file);
+
+        boolean result = true;
+
+        if (CollUtil.isEmpty(fileFilterConfigList)) {
+            return true;
+        }
+
+        for (FileFilterConfig fileFilterConfig : fileFilterConfigList) {
+            String range = fileFilterConfig.getRange();
+            String rule = fileFilterConfig.getRule();
+            String value = fileFilterConfig.getValue();
+
+            FileFilterRangeEnum fileFilterRangeEnum = FileFilterRangeEnum.getEnumByValue(range);
+            if (fileFilterRangeEnum == null) {
+                continue;
+            }
+
+            String content = fileName;
+            switch (fileFilterRangeEnum) {
+                case FILE_NAME:
+                    content = fileName; break;
+                case FILE_CONTENT:
+                    content = fileContent; break;
+                default:
+            }
+
+            FileFilterRuleEnum fileFilterRuleEnum = FileFilterRuleEnum.getEnumByValue(rule);
+            if (fileFilterRuleEnum == null) {
+                continue;
+            }
+            switch (fileFilterRuleEnum) {
+                case CONTAINS:
+                    result = content.contains(value); break;
+                case STARTS_WITH:
+                    result = content.startsWith(value); break;
+                case ENDS_WITH:
+                    result = content.endsWith(value); break;
+                case REGEX:
+                    result = content.matches(value); break;
+                case EQUALS:
+                    result = content.equals(value); break;
+                default:
+            }
+
+        }
+        return result;
+    }
+
+}
